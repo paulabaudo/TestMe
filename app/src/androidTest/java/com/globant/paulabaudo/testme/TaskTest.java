8ff30cf7320c76c4039804187170a78c3daea32f
Paula Baudo <paula.baudo@globant.com>
2015-08-07 11:06:06 -0300

    Some tests using JUnit 3

--- app/src/androidTest/java/com/globant/paulabaudo/testme/TaskTest.java
+++ app/src/androidTest/java/com/globant/paulabaudo/testme/TaskTest.java
@@ -1,6 +1,10 @@
 package com.globant.paulabaudo.testme;
 
-import junit.framework.TestCase;
+import junit.framework.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by paula.baudo on 8/6/2015.
@@ -9,6 +13,15 @@ public class TaskTest extends TestCase {
     final static String TEST_TASK_NAME = "Test task";
     final static String TEST_TASK_DONE_STRING = "Test task: Done";
     final static String TEST_TASK_NOT_DONE_STRING = "Test task: Not done";
+    final static Integer TEST_TASK_TIME_SPENT = 12;
+    final static long CURRENT_DATE = (long) 2548752l;
+
+    public static class TestableTask extends Task{
+        @Override
+        protected Date getCurrentDate() {
+            return new Date(CURRENT_DATE);
+        }
+    }
 
     Task mTask; //sut - System under test
 
@@ -33,5 +46,32 @@ public class TaskTest extends TestCase {
 
         // Assert
         assertEquals("String must use the format <task name>: Done", taskString, TEST_TASK_DONE_STRING);
+    }
+
+    public void testNotDoneStatusIsDisplayedProperly() throws Exception {
+        mTask.setName(TEST_TASK_NAME);
+        mTask.setDone(false);
+
+        assertEquals("String must use the format <task name>: Not done", mTask.toString(), TEST_TASK_NOT_DONE_STRING);
+    }
+
+    public void testTwentyHoursSpent() throws Exception {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(7);
+
+        Integer timeSpent = 0;
+        for (int i = 0; i < numbers.size(); i++){
+            timeSpent += numbers.get(i);
+        }
+
+        assertEquals("Time spent must be 12 hours", TEST_TASK_TIME_SPENT, timeSpent);
+    }
+
+    public void testDoneDateIsSetToCurrentDateWhenTaskIsDone() throws Exception {
+        TestableTask testableTask = new TestableTask();
+        testableTask.setDone(true);
+        assertEquals(testableTask.getDoneDate(), new Date(CURRENT_DATE));
     }
 }
--- app/src/main/java/com/globant/paulabaudo/testme/Task.java
+++ app/src/main/java/com/globant/paulabaudo/testme/Task.java
@@ -1,5 +1,8 @@
 package com.globant.paulabaudo.testme;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by paula.baudo on 8/6/2015.
  */
@@ -7,6 +10,8 @@ public class Task {
 
     private String mName;
     private Boolean mDone;
+    private List<Integer> mTime;
+    private Date mDoneDate;
 
     public Task() {
     }
@@ -25,10 +30,29 @@ public class Task {
 
     public void setDone(Boolean done) {
         mDone = done;
+        if (mDone){
+            mDoneDate = getCurrentDate();
+        }
+    }
+
+    protected Date getCurrentDate(){
+        return new Date();
+    }
+
+    public Date getDoneDate() {
+        return mDoneDate;
     }
 
     @Override
     public String toString() {
         return mName + ": " + (mDone?"Done":"Not done");
+    }
+
+    public List<Integer> getTime() {
+        return mTime;
+    }
+
+    public void setTime(List<Integer> time) {
+        mTime = time;
     }
 }
